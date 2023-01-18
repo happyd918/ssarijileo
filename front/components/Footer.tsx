430aee0ed529ba8a1003f6cc7ba9f206979aabaf
길상욱 <sanguk9805@naver.com>
2023-01-18 13:59:40 +0900

    S08P12B302-16 [Add] add footer

---
+++ front/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <hr />
+      <div className="content has-text-centered">a</div>
+    </footer>
+  );
+}
+
+export default Footer;
