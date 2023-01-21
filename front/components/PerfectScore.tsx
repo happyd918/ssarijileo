81cbbaba28fe11cf812f8387fa92f6ded7d6a814
길상욱 <sanguk9805@naver.com>
2023-01-21 17:02:15 +0900

    S08P12B302-26 [Add] add pitch detector

--- front/components/PerfectScore.tsx
+++ front/components/PerfectScore.tsx
@@ -1,95 +1,128 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+
+import { PitchDetector } from 'pitchy';
+import { useAnimationFrame } from 'hooks';
+import {
+  FFT_SIZE,
+  BUFFER_SIZE,
+  SMOOTHING_TIME_CONSTANT,
+  MIN_DB,
+  NOTE_WINDOW_SIZE,
+} from 'constants/AudioSettings';
 
 import styles from '../styles/PerfectScore.module.scss';
+import { is } from 'immutable';
 
 function PerfectScore() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+  const dataArrayRef = useRef<Float32Array>(new Float32Array(BUFFER_SIZE));
+  const pitchDetectorRef = useRef<PitchDetector<Float32Array>>(
+    PitchDetector.forFloat32Array(BUFFER_SIZE),
+  );
+  const analyserRef = useRef<AnalyserNode>();
+  const noteWindowRef = useRef<number[]>([]);
+  const [isStarted, setIsStarted] = useState(false);
+
+  const start = () => {
+    setIsStarted(true);
+  };
+
+  const stop = () => {
+    setIsStarted(false);
+  };
+
+  useAnimationFrame(
+    (_: number) => {
+      if (
+        !canvasRef.current ||
+        !dataArrayRef.current ||
+        !pitchDetectorRef.current ||
+        !analyserRef.current ||
+        !isStarted
+      )
+        return;
+      const ctx = canvasRef.current.getContext('2d');
+      if (!ctx) return;
+      ctx.fillStyle = '#000000';
+      ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+      const dataArray = dataArrayRef.current;
+      const pitchDetector = pitchDetectorRef.current;
+      const analyser = analyserRef.current;
+
+      analyser.getFloatTimeDomainData(dataArray);
+      const [pitch, clarity] = pitchDetector.findPitch(
+        dataArray,
+        analyser.context.sampleRate,
+      );
+
+      const freqToNote = (freq: number) => {
+        return Math.round(12 * (Math.log(freq / 440.0) / Math.log(2))) + 69;
+      };
+
+      const note = freqToNote(pitch);
+      const noteWindow = noteWindowRef.current;
+      noteWindow.push(note);
+      if (noteWindow.length > NOTE_WINDOW_SIZE) {
+        noteWindow.shift();
+      }
+      const noteCharactorTable = [
+        'C',
+        'C#',
+        'D',
+        'D#',
+        'E',
+        'F',
+        'F#',
+        'G',
+        'G#',
+        'A',
+        'A#',
+        'B',
+      ];
+      let x = 0;
+      const barWidth = canvasRef.current.width / NOTE_WINDOW_SIZE;
+      for (let i = 0; i < noteWindow.length; i++) {
+        if (noteWindow[i] === undefined) {
+          continue;
+        }
+
+        const noteCharactor = noteCharactorTable[noteWindow[i] % 12];
+        if (noteCharactor === undefined) {
+          continue;
+        }
+
+        const octave = Math.floor(noteWindow[i] / 12) - 1;
+
+        const barHeight = noteWindow[i] * 2;
+        const y = canvasRef.current.height - barHeight;
+        ctx.fillStyle = `red`;
+        ctx.fillRect(x, y, barWidth, barHeight);
+        x += barWidth;
+
+        ctx.font = '8px serif';
+        ctx.fillStyle = `white`;
+        ctx.fillText(`${noteCharactor}${octave}`, x, 10);
+      }
+    },
+    [canvasRef, dataArrayRef, pitchDetectorRef, analyserRef, isStarted],
+  );
   useEffect(() => {
-    setCanvas(canvasRef.current);
-  }, []);
+    const audioCtx = new AudioContext();
+    const analyser = audioCtx.createAnalyser();
+    analyserRef.current = analyser;
 
-  const onUser = () => {
-    // 오디오 처리, 주파수 분석 인스턴스
-    const audioContext = new AudioContext();
-    const analyser = audioContext.createAnalyser();
+    analyser.minDecibels = MIN_DB;
+    analyser.smoothingTimeConstant = SMOOTHING_TIME_CONSTANT;
+    analyser.fftSize = FFT_SIZE;
 
-    // 음향 정보 가져오기
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then((stream: MediaStream) => {
-        // 오디오 소스 연결
-        const source = audioContext.createMediaStreamSource(stream);
-        source.connect(analyser);
-        //사운드 출력
-        analyser.connect(audioContext.destination);
-        // 주파수가 담긴 데이터 배열 생성
-        // analyser.fftSize = 2048;
-        // const bufferLength = analyser.frequencyBinCount;
-        // const dataArray = new Uint8Array(bufferLength);
-        const dataArray = new Float32Array(2048);
-        analyser.getFloatTimeDomainData(dataArray);
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+      const source = audioCtx.createMediaStreamSource(stream);
 
-        // FFT를 이용해 주파수를 음정으로 변환
-        const isSilentBuffer = (buffer: Float32Array) => {
-          let N = buffer.length,
-            ret = 0;
-          for (let i = 0; i < N; i++) {
-            ret += buffer[i] * buffer[i];
-          }
-          return (ret = Math.sqrt(ret / N)) < 0.01;
-        };
-
-        const trimBuffer = (buffer: Float32Array, minFreq: number) => {
-          let N = buffer.length,
-            startIdx = 0,
-            endIdx = N - 1;
-          for (let i = 0; i < N / 2; i++) {
-            if (Math.abs(buffer[i]) < minFreq) {
-              startIdx = i;
-              break;
-            }
-          }
-          for (let i = endIdx; i > N / 2; i--) {
-            if (Math.abs(buffer[i]) < minFreq) {
-              endIdx = i;
-              break;
-            }
-          }
-          return buffer.slice(startIdx, endIdx);
-        };
-
-        const getPitch = (data: Float32Array, rate: number) => {
-          if (isSilentBuffer(data)) return 0;
-          let sound = trimBuffer(data, 0.2),
-            N = sound.length,
-            arr = new Array(N).fill(0);
-          for (let i = 0; i < N; i++) {
-            for (let j = 0; j < N; j++) {
-              arr[i] = arr[i] + sound[j] * Math.cos((2 * Math.PI * i * j) / N);
-            }
-          }
-          let downCnt = 0;
-          for (; arr[downCnt] > arr[downCnt + 1]; downCnt++);
-          let maxV = -1,
-            maxIdx = -1;
-          for (let i = downCnt; i < N; i++) {
-            arr[i] > maxV && ((maxV = arr[i]), (maxIdx = i));
-          }
-          let ret = (arr[maxIdx - 1] + arr[maxIdx + 1] - 2 * arr[maxIdx]) / 2,
-            avg = (arr[maxIdx - 1] + arr[maxIdx + 1]) / 2;
-          return (
-            (ret = ret && (maxIdx = maxIdx - avg / (2 * ret))), rate / maxIdx
-          );
-        };
-
-        let pitch = getPitch(dataArray, audioContext.sampleRate);
-      });
-  };
-
-  const onStop = (event: React.MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-  };
+      source.connect(analyser);
+      // analyser.connect(audioCtx.destination);
+    });
+  }, [analyserRef, isStarted]);
 
   return (
     <>
@@ -99,14 +132,14 @@ function PerfectScore() {
         width="512"
         height="256"
         ref={canvasRef}
-      ></canvas>
+      />
       <div id="controls">
-        <input type="button" id="start_button" value="Start" onClick={onUser} />
+        <input type="button" id="start_button" value="Start" onClick={start} />
         &nbsp; &nbsp;
-        <input type="button" id="stop_button" value="Stop" onClick={onStop} />
+        <input type="button" id="stop_button" value="Stop" onClick={stop} />
         <br />
         <br />
-        <output id="msg"></output>
+        <output id="msg" />
       </div>
     </>
   );
