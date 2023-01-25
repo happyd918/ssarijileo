aff5fc8a7e7bc08e0871c2cd70846d7570cad8e7
syg9272 <syg9272@naver.com>
2023-01-25 23:14:47 +0900

    S08P12B302-235 [Modify] modify Login-Modal fe

--- front/components/login/LoginModal.tsx
+++ front/components/login/LoginModal.tsx
@@ -1,5 +1,5 @@
-import styles from '@/styles/LoginModal.module.scss';
 import Image from 'next/image';
+import styles from '@/styles/LoginModal.module.scss';
 
 function LoginModal({ setModalOpen }: any) {
   const closeModal = () => {
