9ef576e8af7b21210765ff7971cbc8b04418ab28
syg9272 <syg9272@naver.com>
2023-01-27 02:56:16 +0900

    S08P12B302-242 [Modify] modify Main-Page fe

--- front/components/common/TopImg.tsx
+++ front/components/common/TopImg.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { useSelector } from 'react-redux';
 import styles from '@/styles/TopImg.module.scss';
 
 function TopImg() {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const storeTheme = useSelector<any>(state => state.theme);
+  useEffect(() => {
+    setThemeMode(localStorage.getItem('theme') || 'light');
+  }, [themeMode, storeTheme]);
+
+  const img = {
+    background: `img/common/${themeMode || 'light'}/${
+      themeMode || 'light'
+    }_background_img.svg`,
+  };
   return (
     <Image
-      src="img/common/light/light_background_img.svg"
+      src={img.background}
       width={798}
       height={701}
       alt="background"
