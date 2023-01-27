c5330286c0097dbb5ebf3fae38905614cc6c12fa
길상욱 <sanguk9805@naver.com>
2023-01-27 21:30:24 +0900

    [Modify] modify todaychart

--- front/components/main/TodayChartItem.tsx
+++ front/components/main/TodayChartItem.tsx
@@ -5,17 +5,18 @@ import styles from '@/styles/TodayChart.module.scss';
 function TodayChartItem(props: {
   item: { rank: number; title: string; singer: string };
 }) {
+  const { item } = props;
   const titleClassName = classNames({
     [styles.songTitle]: true,
-    [styles.isLong]: props.item.title.length > 18,
+    [styles.isLong]: item.title.length > 18,
   });
   return (
     <div className={styles.song}>
       <div className={styles.img}>커버</div>
-      <div className={styles.rank}>{props.item.rank}</div>
+      <div className={styles.rank}>{item.rank}</div>
       <div className={styles.songInfo}>
-        <div className={titleClassName}>{props.item.title}</div>
-        <div className={styles.singer}>{props.item.singer}</div>
+        <div className={titleClassName}>{item.title}</div>
+        <div className={styles.singer}>{item.singer}</div>
       </div>
     </div>
   );
