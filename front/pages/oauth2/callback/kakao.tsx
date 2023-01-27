ffd70990bfe912046e598c6b3dfd9e93085d60de
syg9272 <syg9272@naver.com>
2023-01-28 01:47:25 +0900

    [Modify] modify kakaoLogin fe

--- front/pages/oauth2/callback/kakao.tsx
+++ front/pages/oauth2/callback/kakao.tsx
@@ -13,16 +13,19 @@ function kakao() {
         url: `http://192.168.31.64:8090/login/oauth2/code/kakao?code=${code}`,
       })
         .then(res => {
-          console.log(res); // 토큰이 넘어올 것임
+          console.log(res);
+          // 토큰이 넘어올 것임
           const ACCESS_TOKEN = res.data.accessToken;
-          localStorage.setItem('token', ACCESS_TOKEN); //예시로 로컬에 저장함
-          console.log('성공'); // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
+          //예시로 로컬에 저장함
+          localStorage.setItem('token', ACCESS_TOKEN);
+          console.log('성공');
+          // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
           window.alert('로그인에 성공하였습니다.');
         })
         .catch(err => {
           console.log('소셜로그인 에러', err);
-          window.alert('로그인에 실패하였습니다.');
           // 로그인 실패하면 로그인화면으로 돌려보냄
+          window.alert('로그인에 실패하였습니다.');
         });
     }
     callbackCode();
