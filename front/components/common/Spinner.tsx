c7d78f080dd59de0824bc89c7d1a2798d76d6916
syg9272 <syg9272@naver.com>
2023-01-28 18:37:28 +0900

    [Add] add loadingSpinner fe

--- front/components/common/Spinner.tsx
+++ front/components/common/Spinner.tsx
@@ -3,9 +3,9 @@ import styles from '@/styles/common/Spinner.module.scss';
 function Spinner() {
   return (
     <div className={styles.containner}>
-      <div className={styles.line1}></div>
-      <div className={styles.line2}></div>
-      <div className={styles.line3}></div>
+      <div className={styles.line1} />
+      <div className={styles.line2} />
+      <div className={styles.line3} />
       {/* <div className={styles.spinner}></div> */}
       <div className={styles.context}>loading</div>
     </div>
