cc9ece5cb9fe82635868207a656554990e04555a
syg9272 <syg9272@naver.com>
2023-01-28 18:16:37 +0900

    [Add] add loadingSpinner fe

--- front/components/common/Spinner.tsx
+++ front/components/common/Spinner.tsx
@@ -1,5 +1,15 @@
+import styles from '@/styles/common/Spinner.module.scss';
+
 function Spinner() {
-  return <div>로딩 중 ..</div>;
+  return (
+    <div className={styles.containner}>
+      <div className={styles.line1}></div>
+      <div className={styles.line2}></div>
+      <div className={styles.line3}></div>
+      {/* <div className={styles.spinner}></div> */}
+      <div className={styles.context}>loading</div>
+    </div>
+  );
 }
 
 export default Spinner;
