0f03655c06479d18947bee5445e3a4c513d53b06
길상욱 <sanguk9805@naver.com>
2023-01-29 10:27:53 +0900

    [Modify] modify grid

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -7,7 +7,7 @@ import Header from '@/components/common/Header';
 // import PerfectScore from '@/components/room/PerfectScore';
 // import SoundBar from '@/components/common/SoundBar';
 // import ErrorComp from '@/components/error/ErrorComp';
-import Main from '@/pages/main/main';
+import Main from '@/components/main/main';
 
 import styles from '@/styles/Home.module.scss';
 
