1ce84cec0e5114564c01ab3a1457890ce9591b9b
길상욱 <sanguk9805@naver.com>
2023-01-29 16:44:06 +0900

    [Modify] kakao.init comment

--- front/components/common/Top.tsx
+++ front/components/common/Top.tsx
@@ -1,13 +1,45 @@
-// import Image from 'next/image';
+import { useEffect, useRef } from 'react';
+
 import Title from '@/components/main/Title';
-import styles from '@/styles/common/Top.module.scss';
 import TopImg from './TopImg';
+import { useWave } from '@/hooks/useAnimation';
+
+import styles from '@/styles/common/Top.module.scss';
+import Image from 'next/image';
 
 function Top() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const draw = () => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    const width = canvas.width;
+    const height = canvas.height;
+  };
+
+  useWave(() => {
+    draw();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Title />
       <TopImg />
+      <Image
+        src="img/common/common_microphone_image.svg"
+        width={350}
+        height={350}
+        alt="mic"
+        className={styles.mic}
+      />
+      <canvas
+        className={styles.canvas}
+        ref={canvasRef}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      />
     </div>
   );
 }
--- front/components/main/main.tsx
+++ front/components/main/main.tsx
@@ -28,34 +28,34 @@ function Main() {
     <div className={styles.container}>
       <div className={styles.top}>
         <Top />
-        <Image
-          src="img/common/common_microphone_image.svg"
-          width={350}
-          height={350}
-          alt="mic"
-          className={styles.mic}
-        />
-        <Image
-          src="img/common/common_music_note_image.svg"
-          width={130}
-          height={130}
-          alt="noteA"
-          className={styles.noteA}
-        />
-        <Image
-          src="img/common/common_music_note2_image.svg"
-          width={160}
-          height={160}
-          alt="noteB"
-          className={styles.noteB}
-        />
-        <Image
-          src="img/common/common_music_note3_image.svg"
-          width={120}
-          height={120}
-          alt="noteC"
-          className={styles.noteC}
-        />
+        {/*<Image*/}
+        {/*  src="img/common/common_microphone_image.svg"*/}
+        {/*  width={350}*/}
+        {/*  height={350}*/}
+        {/*  alt="mic"*/}
+        {/*  className={styles.mic}*/}
+        {/*/>*/}
+        {/*<Image*/}
+        {/*  src="img/common/common_music_note_image.svg"*/}
+        {/*  width={130}*/}
+        {/*  height={130}*/}
+        {/*  alt="noteA"*/}
+        {/*  className={styles.noteA}*/}
+        {/*/>*/}
+        {/*<Image*/}
+        {/*  src="img/common/common_music_note2_image.svg"*/}
+        {/*  width={160}*/}
+        {/*  height={160}*/}
+        {/*  alt="noteB"*/}
+        {/*  className={styles.noteB}*/}
+        {/*/>*/}
+        {/*<Image*/}
+        {/*  src="img/common/common_music_note3_image.svg"*/}
+        {/*  width={120}*/}
+        {/*  height={120}*/}
+        {/*  alt="noteC"*/}
+        {/*  className={styles.noteC}*/}
+        {/*/>*/}
       </div>
       <SoundBar />
       <TodayChart />
