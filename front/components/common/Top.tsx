b428ffda0655554fcbf0c32c6776b9058ca65240
길상욱 <sanguk9805@naver.com>
2023-01-30 10:26:37 +0900

    [Modify] cicd error

--- front/components/common/Top.tsx
+++ front/components/common/Top.tsx
@@ -2,22 +2,22 @@ import { useRef } from 'react';
 
 import Title from '@/components/main/Title';
 import TopImg from './TopImg';
-import { useWave } from '@/hooks/useAnimation';
+// import { useWave } from '@/hooks/useAnimation';
 
 import styles from '@/styles/common/Top.module.scss';
 
 function Top() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const draw = () => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const ctx = canvas.getContext('2d');
-    if (!ctx) return;
-  };
-
-  useWave(() => {
-    draw();
-  }, []);
+  // const draw = () => {
+  //   const canvas = canvasRef.current;
+  //   if (!canvas) return;
+  //   const ctx = canvas.getContext('2d');
+  //   if (!ctx) return;
+  // };
+  //
+  // useWave(() => {
+  //   draw();
+  // }, []);
 
   return (
     <div className={styles.container}>
