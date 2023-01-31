effd0fa5630619902d7db5f83b54204590b76629
길상욱 <sanguk9805@naver.com>
2023-01-31 17:59:21 +0900

    [Add] add swiper

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
-import Layout from '@/components/layout/Layout';
-
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import store from '@/redux/store';
