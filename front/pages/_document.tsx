675a87ff563549ffa56f13cb59b0730e33ea9ad3
길상욱 <sanguk9805@naver.com>
2023-02-02 14:25:26 +0900

    [Add] add mypage

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* 카카오 API 사용을 위한 SDK 스크립트 추가 */}
-        <script src="https://developers.kakao.com/sdk/js/kakao.min.js" />
+        {/*<script src="https://developers.kakao.com/sdk/js/kakao.min.js" />*/}
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Miniver&display=swap"
           rel="stylesheet"
