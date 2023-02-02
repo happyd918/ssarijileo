06c3c33912aa63eaaf947a17a67ef65ca885312e
MJun111 <kmj0568@gmail.com>
2023-02-03 03:39:03 +0900

    S08P12B302-473 [Update] Api Gateway 구축

--- back/ssarijileo-apigateway/src/main/java/com/ssafy/ssarijileo/api/apigateway/client/ApiGatewayClient.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.ssarijileo.api.apigateway.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "ApiGatewayClient", url = "localhost:8080/")
-public class ApiGatewayClient {
-
-}
