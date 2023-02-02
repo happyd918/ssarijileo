0b56f44f68ba06e627e86d7a6761e7e7e3077adb
syg9272 <syg9272@naver.com>
2023-02-02 23:23:21 +0900

    [Add] add myPage-Friend fe

--- front/components/mypage/FriendForm.tsx
+++ front/components/mypage/FriendForm.tsx
@@ -63,70 +63,68 @@ function FriendForm() {
     );
   });
   return (
-    <>
-      <div className={styles.contentForm}>
-        <div className={styles.top}>
-          <button className={styles.addBtn}>
-            <Image
-              src="img/common/common_add_image.svg"
-              width={15}
-              height={18}
-              alt="add"
-              className={styles.img}
-            />
-            친구 추가
-          </button>
-          <div className={styles.search}>
-            <input
-              className={styles.input}
-              type="text"
-              placeholder="닉네임을 입력하세요..."
-              onChange={searchFriend}
-            />
-            <Image
-              src="img/common/light/light_common_find_image.svg"
-              width={27}
-              height={27}
-              alt="find"
-              className={styles.find}
-            />
-          </div>
-        </div>
-        <div>
-          <table className={styles.table}>
-            <thead>
-              <tr>
-                <th role="row" />
-                <th>닉네임</th>
-                <th>PLAY횟수</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* 상단 고정 (친구요청) */}
-              <tr className={styles.item}>
-                <td className={styles.profile}>
-                  <div className={styles.content}>
-                    <Image
-                      src="icon/header/dark/dark_profile_icon.svg"
-                      width={30}
-                      height={30}
-                      alt="profile"
-                    />
-                  </div>
-                </td>
-                <td className={styles.name}>누구세요</td>
-                <td className={styles.play}>
-                  <button type="button" className={styles.okBtn}>
-                    친구수락
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-            <tbody className={styles.friendList}>{listItems}</tbody>
-          </table>
+    <div className={styles.contentForm}>
+      <div className={styles.top}>
+        <button type="button" className={styles.addBtn}>
+          <Image
+            src="img/common/common_add_image.svg"
+            width={15}
+            height={18}
+            alt="add"
+            className={styles.img}
+          />
+          친구 추가
+        </button>
+        <div className={styles.search}>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="닉네임을 입력하세요..."
+            onChange={searchFriend}
+          />
+          <Image
+            src="img/common/light/light_common_find_image.svg"
+            width={27}
+            height={27}
+            alt="find"
+            className={styles.find}
+          />
         </div>
       </div>
-    </>
+      <div>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th role="row" />
+              <th>닉네임</th>
+              <th>PLAY횟수</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* 상단 고정 (친구요청) */}
+            <tr className={styles.item}>
+              <td className={styles.profile}>
+                <div className={styles.content}>
+                  <Image
+                    src="icon/header/dark/dark_profile_icon.svg"
+                    width={30}
+                    height={30}
+                    alt="profile"
+                  />
+                </div>
+              </td>
+              <td className={styles.name}>누구세요</td>
+              <td className={styles.play}>
+                <button type="button" className={styles.okBtn}>
+                  친구수락
+                </button>
+              </td>
+            </tr>
+          </tbody>
+          <tbody className={styles.friendList}>{listItems}</tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
