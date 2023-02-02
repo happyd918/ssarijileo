3434ca80a205593ab6a5d08c7799a86e4b49573d
MJun111 <kmj0568@gmail.com>
2023-02-03 03:39:06 +0900

    S08P12B302-473 [Update] Api Gateway 구축

--- back/ssarijileo-eureka/src/main/java/com/ssafy/ssarijileo/SsarijileoEurekaApplication.java
+++ back/ssarijileo-eureka/src/main/java/com/ssafy/ssarijileo/SsarijileoEurekaApplication.java
@@ -3,14 +3,14 @@ package com.ssafy.ssarijileo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@EnableDiscoveryClient
-public class SsarijileoApiGatewayApplication {
+@EnableEurekaServer
+public class SsarijileoEurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SsarijileoApiGatewayApplication.class, args);
+		SpringApplication.run(SsarijileoEurekaApplication.class, args);
 	}
 
 }
