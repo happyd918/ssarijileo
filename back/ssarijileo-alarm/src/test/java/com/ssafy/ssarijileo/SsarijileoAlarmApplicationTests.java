4678c335266c51cd39250671389afb00d7eeeb76
leesumin <sumin940305@gmail.com>
2023-02-06 00:56:40 +0900

    [Add] DockerFile 추가

--- back/ssarijileo-alarm/src/test/java/com/ssafy/ssarijileo/SsarijileoAlarmApplicationTests.java
+++ back/ssarijileo-alarm/src/test/java/com/ssafy/ssarijileo/SsarijileoAlarmApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.ssarijileo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SsarijileoAlarmApplicationTests {
 
 	@Test
