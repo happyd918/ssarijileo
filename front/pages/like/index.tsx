c5b61e0596783d26f4eff8868e8866b5e0e61302
syg9272 <syg9272@naver.com>
2023-02-05 20:15:53 +0900

    [Modify] modify like page

--- front/pages/like/index.tsx
+++ front/pages/like/index.tsx
@@ -4,6 +4,7 @@ import styles from '@/styles/like/Like.module.scss';
 
 import Music from '@/components/like/Music';
 import Video from '@/components/like/Video';
+import SoundBar from '@/components/common/SoundBar';
 
 function Like() {
   const [type, setType] = useState('찜목록');
@@ -16,35 +17,38 @@ function Like() {
     [styles.isSelect]: type === '녹화본',
   });
   return (
-    <div className={styles.container}>
-      <div className={styles.top}>
-        <div className={styles.title}>마이뮤직</div>
-        <div className={styles.menu}>
-          <button
-            type="button"
-            onClick={() => {
-              setType('찜목록');
-            }}
-            className={musicClass}
-          >
-            찜목록
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              setType('녹화본');
-            }}
-            className={videoClass}
-          >
-            녹화본
-          </button>
+    <>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <div className={styles.title}>마이뮤직</div>
+          <div className={styles.menu}>
+            <button
+              type="button"
+              onClick={() => {
+                setType('찜목록');
+              }}
+              className={musicClass}
+            >
+              찜목록
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setType('녹화본');
+              }}
+              className={videoClass}
+            >
+              녹화본
+            </button>
+          </div>
+        </div>
+        <div className={styles.list}>
+          {type === '찜목록' && <Music />}
+          {type === '녹화본' && <Video />}
         </div>
       </div>
-      <div className={styles.list}>
-        {type === '찜목록' && <Music />}
-        {type === '녹화본' && <Video />}
-      </div>
-    </div>
+      <SoundBar />
+    </>
   );
 }
 
