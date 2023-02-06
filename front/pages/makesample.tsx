933f99378409c8790d091eecaa803308764be2e6
길상욱 <sanguk9805@naver.com>
2023-02-06 23:58:54 +0900

    [Add] animation

--- front/pages/makesample.tsx
+++ front/pages/makesample.tsx
@@ -2,47 +2,44 @@ import React, { useRef, useEffect, useState } from 'react';
 
 import { PitchDetector } from 'pitchy';
 import { useCanvas } from '@/hooks/useCanvas';
-import { useAnimation } from '@/hooks/useAnimation';
 import * as data from '@/constants/PerfectScoreData';
 
 import styles from '@/styles/MakeSample.module.scss';
+import { useAnimation } from '@/hooks/useAnimation';
 
 function MakeSample() {
-  const dataArrayRef = useRef<Float32Array>(new Float32Array(8192));
+  const [volume, setVolume] = useState(0.5);
+  const [tempo, setTempo] = useState(1);
+  const dataArrayRef = useRef<Float32Array>(new Float32Array(data.BUFFER_SIZE));
   const pitchDetectorRef = useRef<PitchDetector<Float32Array>>(
-    PitchDetector.forFloat32Array(8192),
+    PitchDetector.forFloat32Array(data.BUFFER_SIZE),
   );
   const analyserRef = useRef<AnalyserNode>();
   const gainRef = useRef<GainNode>();
   const sourceRef = useRef<AudioBufferSourceNode>();
-  const startTimeRef = useRef<number>(0);
   const noteWindowRef = useRef<number[]>([]);
-  const drawWindowRef = useRef<number[]>([]);
-  const dbRef = useRef<
-    {
-      note: number;
-      time: number;
-      cnt: number;
-    }[]
-  >([]);
   const [isReady, setIsReady] = useState(false);
   const [isStarted, setIsStarted] = useState(false);
-
-  const save = () => {
-    const jsonData = JSON.stringify(dbRef.current);
-    console.log(jsonData);
-  };
 
   const start = () => {
     setIsStarted(true);
     sourceRef.current?.start();
-    startTimeRef.current = performance.now();
   };
 
   const stop = () => {
     sourceRef.current?.stop();
     setIsStarted(false);
-    save();
+    console.log(noteWindowRef.current);
+  };
+
+  const changeVolume = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVolume(Number(e.target.value));
+    gainRef.current?.gain.setValueAtTime(Number(e.target.value), 0);
+  };
+
+  const changeTempo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTempo(Number(e.target.value));
+    sourceRef.current?.playbackRate.setValueAtTime(Number(e.target.value), 0);
   };
 
   const isSilentBuffer = (buffer: Float32Array) => {
@@ -50,13 +47,14 @@ function MakeSample() {
     for (let i = 0; i < buffer.length; i++) {
       ret += buffer[i] * buffer[i];
     }
-    return Math.sqrt(ret / buffer.length) < 0.01;
+    return Math.sqrt(ret / buffer.length) < data.SILENCE_THRESHOLD;
   };
 
+  // 메인 로직
   const canvasWidth = 10000;
   const canvasHeight = 600;
   const canvasRef = useCanvas(canvasWidth, canvasHeight);
-  const changeToJson = () => {
+  const play = () => {
     if (
       !dataArrayRef.current ||
       !pitchDetectorRef.current ||
@@ -64,7 +62,6 @@ function MakeSample() {
       !isStarted
     )
       return;
-
     const ctx = canvasRef.current?.getContext('2d');
     if (!ctx) return;
     ctx.fillStyle = '#000';
@@ -88,29 +85,14 @@ function MakeSample() {
     let note = freqToNote(pitch);
     if (note < 40 || note > 90) note = -1;
     const noteWindow = noteWindowRef.current;
-    const drawWindow = drawWindowRef.current;
-    if (note === undefined || Number.isNaN(note)) note = -1;
-    if (note !== noteWindow[noteWindow.length - 1]) {
-      const deltaTime = parseFloat(
-        ((performance.now() - startTimeRef.current) / 1000).toFixed(2),
-      );
-      const noteData = {
-        note: noteWindow[noteWindow.length - 1] || -1,
-        time: deltaTime,
-        cnt: noteWindow.length,
-      };
-      dbRef.current.push(noteData);
-      noteWindow.splice(0, noteWindow.length);
-    }
     noteWindow.push(note);
-    drawWindow.push(note);
 
     // 음정 출력
     let x = 0;
     const barWidth = 0.5;
-    for (let i = 0; i < drawWindow.length; i++) {
+    for (let i = 0; i < noteWindow.length; i++) {
       const barHeight = 10;
-      const y = canvasHeight - drawWindow[i] * 5;
+      const y = canvasHeight - noteWindow[i] * 5;
       if (!Number.isNaN(y)) {
         const gradient = ctx.createLinearGradient(
           x,
@@ -126,16 +108,16 @@ function MakeSample() {
       ctx.beginPath();
       if (
         i !== 0 &&
-        i !== drawWindow.length - 1 &&
-        drawWindow[i] !== drawWindow[i - 1] &&
-        drawWindow[i] !== drawWindow[i + 1]
+        i !== noteWindow.length - 1 &&
+        noteWindow[i] !== noteWindow[i - 1] &&
+        noteWindow[i] !== noteWindow[i + 1]
       ) {
         ctx.roundRect(x, y, barWidth + 1, barHeight, [5, 5, 5, 5]);
-      } else if (i !== 0 && drawWindow[i] !== drawWindow[i - 1]) {
+      } else if (i !== 0 && noteWindow[i] !== noteWindow[i - 1]) {
         ctx.roundRect(x, y, barWidth + 1, barHeight, [5, 0, 0, 5]);
       } else if (
-        i !== drawWindow.length - 1 &&
-        drawWindow[i] !== drawWindow[i + 1]
+        i !== noteWindow.length - 1 &&
+        noteWindow[i] !== noteWindow[i + 1]
       ) {
         ctx.roundRect(x, y, barWidth + 1, barHeight, [0, 5, 5, 0]);
       } else {
@@ -146,7 +128,7 @@ function MakeSample() {
     }
   };
 
-  useAnimation(changeToJson, 0, [
+  useAnimation(play, 0, [
     dataArrayRef,
     pitchDetectorRef,
     analyserRef,
@@ -161,8 +143,8 @@ function MakeSample() {
     analyserRef.current = analyser;
     gainRef.current = gainNode;
 
-    analyser.minDecibels = -90;
-    analyser.smoothingTimeConstant = 0.5;
+    analyser.minDecibels = data.MIN_DB;
+    analyser.smoothingTimeConstant = data.SMOOTHING_TIME_CONSTANT;
     analyser.fftSize = 8192;
 
     fetch('sounds/voice.mp3')
@@ -174,10 +156,9 @@ function MakeSample() {
         source.connect(analyser);
         source.connect(gainNode);
         gainNode.connect(audioCtx.destination);
-
         sourceRef.current = source;
         source.onended = () => {
-          save();
+          console.log(noteWindowRef.current);
         };
         setIsReady(sourceRef.current !== undefined);
       });
@@ -187,8 +168,8 @@ function MakeSample() {
     <>
       <canvas
         className={styles.canvas}
-        width={canvasWidth}
-        height={canvasHeight}
+        width={10000}
+        height={600}
         ref={canvasRef}
       />
       <div id="controls">
@@ -208,6 +189,24 @@ function MakeSample() {
           value="Stop"
           onClick={stop}
           disabled={!isStarted}
+        />
+        <input
+          type="range"
+          min={0}
+          max={1}
+          color="gray"
+          step={0.01}
+          value={volume}
+          onChange={changeVolume}
+        />
+        <input
+          className="playback-rate-control"
+          type="range"
+          min="0.25"
+          max="3"
+          step="0.05"
+          value={tempo}
+          onChange={changeTempo}
         />
       </div>
     </>
