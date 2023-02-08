79816ef8c33547c76b5a644668d6a15ef1405097
길상욱 <sanguk9805@naver.com>
2023-02-08 14:39:59 +0900

    [Modify] error

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -20,97 +20,95 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      data: {
-        chartItemA: [
-          {
-            rank: 1,
-            title: 'Ditto',
-            singer: 'NewJeans',
-          },
-          {
-            rank: 2,
-            title: '사건의 지평선',
-            singer: '윤하',
-          },
-          {
-            rank: 3,
-            title: 'Hype boy',
-            singer: 'NewJeans',
-          },
-          {
-            rank: 4,
-            title: 'OMG',
-            singer: 'NewJeans',
-          },
-          {
-            rank: 5,
-            title: 'After LIKE',
-            singer: 'IVE(아이브)',
-          },
-        ],
-        chartItemB: [
-          {
-            rank: 6,
-            title: 'ANTIFRAGILE',
-            singer: 'LE SSERAFIM (르세라핌)',
-          },
-          {
-            rank: 7,
-            title: 'Attention',
-            singer: 'NewJeans',
-          },
-          {
-            rank: 8,
-            title: 'LOVE DIVE',
-            singer: 'IVE(아이브)',
-          },
-          {
-            rank: 9,
-            title: 'Nxde',
-            singer: '여자(아이들)',
-          },
-          {
-            rank: 10,
-            title: 'NOT SORRY (Feat. pH-1) (Prod. by Slom)',
-            singer: '이영지',
-          },
-        ],
-        ranking: [
-          {
-            rank: 1,
-            medal: 'img/main/main_medal_gold_image.svg',
-            profile: 'icon/header/light/light_profile_icon.svg',
-            name: '나는이수민',
-            title: 'Hype boy',
-            singer: 'NewJeans',
-            like: '4k',
-          },
-          {
-            rank: 2,
-            medal: 'img/main/main_medal_sliver_image.svg',
-            profile: 'icon/header/light/light_profile_icon.svg',
-            name: '김맹준',
-            title: 'Hype boy',
-            singer: 'NewJeans',
-            like: '4k',
-          },
-          {
-            rank: 3,
-            medal: 'img/main/main_medal_bronze_image.svg',
-            profile: 'icon/header/light/light_profile_icon.svg',
-            name: 'zㅣ존예지',
-            title: 'Hype boy',
-            singer: 'NewJeans',
-            like: '4k',
-          },
-        ],
-      },
+      chartItemA: [
+        {
+          rank: 1,
+          title: 'Ditto',
+          singer: 'NewJeans',
+        },
+        {
+          rank: 2,
+          title: '사건의 지평선',
+          singer: '윤하',
+        },
+        {
+          rank: 3,
+          title: 'Hype boy',
+          singer: 'NewJeans',
+        },
+        {
+          rank: 4,
+          title: 'OMG',
+          singer: 'NewJeans',
+        },
+        {
+          rank: 5,
+          title: 'After LIKE',
+          singer: 'IVE(아이브)',
+        },
+      ],
+      chartItemB: [
+        {
+          rank: 6,
+          title: 'ANTIFRAGILE',
+          singer: 'LE SSERAFIM (르세라핌)',
+        },
+        {
+          rank: 7,
+          title: 'Attention',
+          singer: 'NewJeans',
+        },
+        {
+          rank: 8,
+          title: 'LOVE DIVE',
+          singer: 'IVE(아이브)',
+        },
+        {
+          rank: 9,
+          title: 'Nxde',
+          singer: '여자(아이들)',
+        },
+        {
+          rank: 10,
+          title: 'NOT SORRY (Feat. pH-1) (Prod. by Slom)',
+          singer: '이영지',
+        },
+      ],
+      ranking: [
+        {
+          rank: 1,
+          medal: 'img/main/main_medal_gold_image.svg',
+          profile: 'icon/header/light/light_profile_icon.svg',
+          name: '나는이수민',
+          title: 'Hype boy',
+          singer: 'NewJeans',
+          like: '4k',
+        },
+        {
+          rank: 2,
+          medal: 'img/main/main_medal_sliver_image.svg',
+          profile: 'icon/header/light/light_profile_icon.svg',
+          name: '김맹준',
+          title: 'Hype boy',
+          singer: 'NewJeans',
+          like: '4k',
+        },
+        {
+          rank: 3,
+          medal: 'img/main/main_medal_bronze_image.svg',
+          profile: 'icon/header/light/light_profile_icon.svg',
+          name: 'zㅣ존예지',
+          title: 'Hype boy',
+          singer: 'NewJeans',
+          like: '4k',
+        },
+      ],
     },
   };
 }
 
 function Home(props: any) {
-  const { chartItemA, chartItemB, ranking } = props.data;
+  const { chartItemA, chartItemB, ranking } = props;
   const [themeMode, setThemeMode] = useState('light');
   const storeTheme: any = useSelector<any>(state => state.theme);
 
