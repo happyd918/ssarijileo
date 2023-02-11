c4b4338ee910587a88e85d3e24660ac6b2aafa30
leesumin <sumin940305@gmail.com>
2023-02-11 18:17:05 +0900

    [Modify] Feign localhost 수정

--- back/ssarijileo-session/src/main/java/com/ssafy/ssarijileo/api/singing/client/SingingClient.java
+++ back/ssarijileo-session/src/main/java/com/ssafy/ssarijileo/api/singing/client/SingingClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import com.ssafy.ssarijileo.api.singing.dto.SingingDto;
 import com.ssafy.ssarijileo.test.client.BaseResponseBody;
 
-@FeignClient(name = "singing-client", url = "localhost:8080/api/v1/singing")
+@FeignClient(name = "singing-client", url = "business:8080/api/v1/singing")
 public interface SingingClient {
 
 	ResponseEntity<? extends BaseResponseBody> insertSinging(@RequestHeader String userId, @RequestBody SingingDto singingDto);
