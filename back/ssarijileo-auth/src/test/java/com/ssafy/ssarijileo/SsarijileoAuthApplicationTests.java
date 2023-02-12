a7aa613fa9cb5ce0f750d41e6ed12c54be79e44e
leesumin <sumin940305@gmail.com>
2023-02-13 00:10:34 +0900

    [Modify] 테스트용 주석 추가

--- back/ssarijileo-auth/src/test/java/com/ssafy/ssarijileo/SsarijileoAuthApplicationTests.java
+++ back/ssarijileo-auth/src/test/java/com/ssafy/ssarijileo/SsarijileoAuthApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 // @SpringBootTest
+// test용 주석 추가
 class SsarijileoAuthApplicationTests {
 
 	@Test
