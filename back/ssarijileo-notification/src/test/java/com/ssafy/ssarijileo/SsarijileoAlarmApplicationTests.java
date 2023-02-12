45774132a627727b71e5c1db23ec5037b2f0de56
leesumin <sumin940305@gmail.com>
2023-02-13 00:07:11 +0900

    [Modify] test용 주석 추가

--- back/ssarijileo-notification/src/test/java/com/ssafy/ssarijileo/SsarijileoAlarmApplicationTests.java
+++ back/ssarijileo-notification/src/test/java/com/ssafy/ssarijileo/SsarijileoAlarmApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 // @SpringBootTest
+// test 용도 주석 추가
 class SsarijileoAlarmApplicationTests {
 
 	@Test
