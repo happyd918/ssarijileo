7ee8f9f983d4d7d377ce40a3887c82d2edcfc981
길상욱 <sanguk9805@naver.com>
2023-02-13 16:59:56 +0900

    [Modify] error

--- front/components/like/Music.tsx
+++ front/components/like/Music.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useSelector } from 'react-redux';
-import styles from '@/styles/like/Music.module.scss';
+
 // import ChartListItem from '@/components/chart/ChartListItem';
 import Pagination from '@/components/common/Pagination';
+
+import styles from '@/styles/like/Music.module.scss';
 
 function Music() {
   // 노래 배열도 상태관리 (좋아요 여부 변경 해야 함!!!)
