f2285399eaf5ceb3b1d05c7610da76da6a006201
syg9272 <syg9272@naver.com>
2023-02-16 01:33:47 +0900

    [Modify] modify error

--- front/components/room/Guess.tsx
+++ front/components/room/Guess.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from '@/styles/room/Guess.module.scss';
 
 function Guess(props: { session: any; nextSong: any }) {
-  const { nextSong } = props;
+  const { session, nextSong } = props;
   const lyricList = nextSong.lyricsList.map((item: any) => {
     // setInterval로 다음 가사 시간 전까지 class지정
     return (
@@ -22,6 +22,20 @@ function Guess(props: { session: any; nextSong: any }) {
             width={50}
             height={50}
             alt="o"
+            onClick={() => {
+              session
+                .signal({
+                  data: 'ok', // Any string (optional)
+                  to: [], // Array of Connection objects (optional. Broadcast to everyone if empty)
+                  type: 'ok', // The type of message (optional)
+                })
+                .then(() => {
+                  console.log(`ok button 정보 송신 성공`);
+                })
+                .catch((error: any) => {
+                  console.error('ok button 정보 송신 실패', error);
+                });
+            }}
           />
           <Image
             className={styles.x}
@@ -29,6 +43,20 @@ function Guess(props: { session: any; nextSong: any }) {
             width={50}
             height={50}
             alt="x"
+            onClick={() => {
+              session
+                .signal({
+                  data: 'no', // Any string (optional)
+                  to: [], // Array of Connection objects (optional. Broadcast to everyone if empty)
+                  type: 'no', // The type of message (optional)
+                })
+                .then(() => {
+                  console.log(`no button 정보 송신 성공`);
+                })
+                .catch((error: any) => {
+                  console.error('no button 정보 송신 실패', error);
+                });
+            }}
           />
         </div>
       </div>
