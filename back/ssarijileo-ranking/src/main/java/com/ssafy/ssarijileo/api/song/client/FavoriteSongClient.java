5f50324648eb549e232a57dc608af56761c2ed46
MyeongjunKim <kmj0568@gmail.com>
2023-02-15 09:59:59 +0900

    [Update] 수정

--- back/ssarijileo-ranking/src/main/java/com/ssafy/ssarijileo/api/song/client/FavoriteSongClient.java
+++ back/ssarijileo-ranking/src/main/java/com/ssafy/ssarijileo/api/song/client/FavoriteSongClient.java
@@ -3,7 +3,8 @@ package com.ssafy.ssarijileo.api.song.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "favorite-song-client", url = "192.168.49.2:31000/api/v1/favorite-song")
+// @FeignClient(name = "favorite-song-client", url = "192.168.49.2:31000/api/v1/favorite-song")
+@FeignClient(name = "favorite-song-client", url = "localhost:8080/api/v1/favorite-song")
 public interface FavoriteSongClient {
 
     @GetMapping
