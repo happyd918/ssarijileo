948de4ecababc6677c51c1d5b85e41984be7d4cb
서예지 <syg9272@naver.com>
2023-02-16 11:33:50 +0900

    [Modify] modify error

--- front/components/room/SingerScreen.tsx
+++ front/components/room/SingerScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import classnames from 'classnames';
 import { useDispatch, useSelector } from 'react-redux';
 import styles from '@/styles/room/Screen.module.scss';
