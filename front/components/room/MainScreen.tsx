9c16504d544e1d0bb61aa2d13c13482d54372fd2
길상욱 <sanguk9805@naver.com>
2023-02-17 05:19:38 +0900

    [Modify] error

--- front/components/room/MainScreen.tsx
+++ front/components/room/MainScreen.tsx
@@ -151,33 +151,33 @@ export function MainScreen(props: {
 
   const videoRecorderRef = useRef<MediaRecorder>();
 
-  const mergeAudioStreams = (
-    musicStream: MediaStream,
-    voiceStream: MediaStream,
-  ) => {
-    // 비디오, 오디오스트림 연결
-    const context = new AudioContext();
-    const destination = context.createMediaStreamDestination();
-    let hasDesktop = false;
-    let hasVoice = false;
-    if (musicStream && musicStream.getAudioTracks().length > 0) {
-      const source1 = context.createMediaStreamSource(musicStream);
-      const desktopGain = context.createGain();
-      desktopGain.gain.value = 0.5;
-      source1.connect(desktopGain).connect(destination);
-      hasDesktop = true;
-    }
-
-    if (voiceStream && voiceStream.getAudioTracks().length > 0) {
-      const source2 = context.createMediaStreamSource(voiceStream);
-      const voiceGain = context.createGain();
-      voiceGain.gain.value = 1.0;
-      source2.connect(voiceGain).connect(destination);
-      hasVoice = true;
-    }
-
-    return hasDesktop || hasVoice ? destination.stream.getAudioTracks() : [];
-  };
+  // const mergeAudioStreams = (
+  //   musicStream: MediaStream,
+  //   voiceStream: MediaStream,
+  // ) => {
+  //   // 비디오, 오디오스트림 연결
+  //   const context = new AudioContext();
+  //   const destination = context.createMediaStreamDestination();
+  //   let hasDesktop = false;
+  //   let hasVoice = false;
+  //   if (musicStream && musicStream.getAudioTracks().length > 0) {
+  //     const source1 = context.createMediaStreamSource(musicStream);
+  //     const desktopGain = context.createGain();
+  //     desktopGain.gain.value = 0.5;
+  //     source1.connect(desktopGain).connect(destination);
+  //     hasDesktop = true;
+  //   }
+  //
+  //   if (voiceStream && voiceStream.getAudioTracks().length > 0) {
+  //     const source2 = context.createMediaStreamSource(voiceStream);
+  //     const voiceGain = context.createGain();
+  //     voiceGain.gain.value = 1.0;
+  //     source2.connect(voiceGain).connect(destination);
+  //     hasVoice = true;
+  //   }
+  //
+  //   return hasDesktop || hasVoice ? destination.stream.getAudioTracks() : [];
+  // };
 
   // 화면 공유
   const screenShare = (
