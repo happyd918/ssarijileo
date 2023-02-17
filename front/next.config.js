7021b8b70f68e5ac6e30cc78e34aa0cfb28936d7
길상욱 <sanguk9805@naver.com>
2023-02-17 12:02:13 +0900

    [Modify] error

--- front/next.config.js
+++ front/next.config.js
@@ -1,32 +1,42 @@
 const nextConfig = {
   images: {
     domains: [
-      "i1.sndcdn.com",
-      "image.genie.co.kr",
-      "image.bugsm.co.kr",
-      "k.kakaocdn.net",
+      'i1.sndcdn.com',
+      'image.genie.co.kr',
+      'image.bugsm.co.kr',
+      'k.kakaocdn.net',
     ],
   },
   reactStrictMode: false,
   async rewrites() {
     return [
       {
-        source: "/api/v1/sse/:path*",
-        destination: "http://i8b302.p.ssafy.io:8060/api/v1/sse/:path*",
+        source: '/api/v1/sse/:path*',
+        destination: 'http://i8b302.p.ssafy.io:8060/api/v1/sse/:path*',
       },
       {
-        source: "/api/v1/friend/invite/:path*",
+        source: '/api/v1/friend/invite/:path*',
         destination:
-          "http://i8b302.p.ssafy.io:8060/api/v1/friend/invite/:path*",
+          'http://i8b302.p.ssafy.io:8060/api/v1/friend/invite/:path*',
       },
       {
-        source: "/api/v1/friend/request/:path*",
+        source: '/api/v1/friend/request/:path*',
         destination:
-          "http://i8b302.p.ssafy.io:8060/api/v1/friend/request/:path*",
+          'http://i8b302.p.ssafy.io:8060/api/v1/friend/request/:path*',
       },
+      // {
+      //   source: '/song-file/:path*',
+      //   destination:
+      //     'https://ssafy-ssarijileo.s3.ap-northeast-2.amazonaws.com/song-file/:path*',
+      // },
+      // {
+      //   source: '/note-file/:path*',
+      //   destination:
+      //     'https://ssafy-ssarijileo.s3.ap-northeast-2.amazonaws.com/note-file/:path*',
+      // },
       {
-        source: "/api/v1/:path*",
-        destination: "http://i8b302.p.ssafy.io:8000/api/v1/:path*",
+        source: '/api/v1/:path*',
+        destination: 'http://i8b302.p.ssafy.io:8000/api/v1/:path*',
       },
     ];
   },
