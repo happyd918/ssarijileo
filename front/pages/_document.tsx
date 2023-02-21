1cc7076ba0261d2f5d7457dec28ca9d26b7e9b46
길상욱 <sanguk9805@naver.com>
2023-02-22 08:50:15 +0900

    Update front/pages/_document.tsx

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head>
         {/* 카카오 API 사용을 위한 SDK 스크립트 추가 */}
         <script src="https://developers.kakao.com/sdk/js/kakao.min.js" />
